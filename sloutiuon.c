a71b41737e73a9114867603f3f5166c029b11bae
Fahim Faisal <fahimfaisal.soikot@gmial.com>
2020-10-19 20:46:41 +0600

    URI Beginner Solution 1001 - 1008

---
+++ sloutiuon.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main(void) {
+
+//* Problem 1001 - Extremely Basic
+    // int A, B, X;
+    // scanf("%d %d", &A, &B);
+    // X = A + B;
+    // printf("X = %d\n", X);
+
+//* Problem 1002 - Area of a Circle
+    // double A, R, n;
+    // scanf("%lf", &R);
+    // n = 3.14159;
+    // A = n * (R * R);
+    // printf("A=%.4lf\n", A);
+
+//* Problem 1003 - Simple Sum
+    // int A, B, SOMA;
+    // scanf("%d %d", &A, &B);
+    // SOMA = A + B
+    // printf("SOMA = %d\n", SOMA);
+
+//* Problem 1004 - Simple Product
+    // int A, B, PROD;
+    // scanf("%d %d", &A, &B);
+    // PROD = A * B;
+    // printf("PROD = %d\n", PROD);
+
+//* Problem 1005 - Average 1
+    // float A, B, med;
+    // scanf("%f %f", &A, &B);
+    // med = ((A * 3.5) + (B * 7.5)) / (3.5 + 7.5);
+    // printf("MEDIA = %.5f\n", med);
+
+//* Problem 1006 - Average 2
+    // float A, B, C, med;
+    // scanf("%f %f %f", &A, &B, &C);
+    // med = ((A * 2) + (B * 3) + (C * 5)) / (2 + 3 + 5);
+    // printf("MEDIA = %.1f\n", med);
+
+//* Problem 1007 - Difference
+    // int A, B, C, D, DIFERENCA;
+    // scanf("%d %d %d %d", &A, &B, &C, &D);
+    // DIFERENCA = (A * B - C * D);
+    // printf("DIFERENCA = %d\n", DIFERENCA);
+
+//* Problem 1008 - Salary
+    // int employees_number, worked_hours_number;
+    // float hourly_amount, SALARY;
+    // scanf("%d %d %f", &employees_number, &worked_hours_number, &hourly_amount);
+    // printf("NUMBER = %d\n", employees_number);
+    // SALARY = worked_hours_number * hourly_amount;
+    // printf("SALARY = U$ %.2f\n",SALARY);
+
+    return 0;
+}
