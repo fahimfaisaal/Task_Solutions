8793e9ea18a7c28591b93dc12bc0aa031bef2204
Fahim Faisal <fahimfaisal.soikot@gmial.com>
2020-10-31 14:14:57 +0600

    some fix bugs

---
+++ Solution_1038-1073/solution.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+
+//* Problem 1038 - Snack
+    // int product_code, quantity;
+    // char str[] = "Total: R$";
+    // scanf("%d %d", &product_code, &quantity);
+
+    // if (product_code == 1) {
+    //     printf("%s %.2f\n", str, 4.00 * quantity);
+    // } else if (product_code == 2) {
+    //     printf("%s %.2f\n", str, 4.50 * quantity);
+    // } else if (product_code == 3) {
+    //     printf("%s %.2f\n", str, 5.00 * quantity);
+    // }else if (product_code == 4) {
+    //     printf("%s %.2f\n", str, 2.00 * quantity);
+    // } else if (product_code == 5) {
+    //     printf("%s %.2f\n", str, 1.50 * quantity);
+    // }
+
+}
