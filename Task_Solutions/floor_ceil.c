3283d96408c46a5b186d6a5828ef1876a73ea610
Fahim Faisal <fahimfaisal.soikot@gmial.com>
2020-11-02 10:39:06 +0600

    fix some bug's in my_floor function

--- Task_Solutions/floor_ceil.c
+++ Task_Solutions/floor_ceil.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
-// #include <math.h>
+#include <math.h>
 
 //* Implement of floor function
-float my_floor(float num) {
+float my_floor(double num) {
     int float_to_int = num;
-    float int_to_float = (float_to_int * 1.00);
+    double int_to_float = (float_to_int * 1.00);
+    double floating_num =  (num - int_to_float);
+
+    if (floating_num < 0.0) int_to_float -= 1;
     return int_to_float;
 }
 
 //* Implement of ceil function
-float my_ceil(float num) {
-    float int_to_float, floating_num;
+float my_ceil(double num) {
+    double int_to_float, floating_num;
     int float_to_int = num;
     floating_num = (num - float_to_int);
     
@@ -23,11 +26,11 @@ float my_ceil(float num) {
 }
 
 int main (void) {
-    float a = 145.100;
+    float a = -145.100;
 
     float get_floor = my_floor(a); //* also same output in builtin floor function
     printf("The floor number is = %f\n", get_floor);
 
     float get_ceil = my_ceil(a); //* also same output in builtin ceil function
-    printf("The ceil number is = %f\n", get_ceil);
+    printf("The ceil number is = %lf\n", get_ceil);
 }
