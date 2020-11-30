c9c2a4053b0ae0c648721325ce51af78f739cfbe
Fahim Faisal <fahimfaisal.soikot@gmial.com>
2020-11-30 08:19:11 +0600

    fix some bugs

--- Task_Solutions/array/array.c
+++ Task_Solutions/array/array.c
@@ -2,49 +2,50 @@
 
 int main() {
     int arr[100], position[100];
-    int positionSize = 0;
+    int positionIndex = 0;
     int indexNumber;
     int value;
 
     while (1)  {
 
-        int validationIndex = 0;
-        // index form user
-        printf("enter the index number : ");
+        int isValidIndex = 0;
+        
+        //* Index form user
+        printf("Enter the index number : ");
         scanf("%d", &indexNumber);
 
-        // no validation break
+        //* Loop break condition
         if (indexNumber < 0) {
-            printf("Breaking loop\n");
+            printf("\nBreaking loop\n\n");
             break;
         }
 
+        //* Array range condition
         if (indexNumber >= 100) {
-            printf("array index out of bound\n");
+            printf("\nArray index out of bound!\n\n");
             continue;
         }
 
         //* Validation of index numbers
-        for (int i = 0; i < positionSize; i++) {
+        for (int i = 0; i < positionIndex; i++) {
             if (indexNumber == position[i]) {
-                printf("You've already assigned this index number -> %d with value -> %d, Please assign a new index number.\n\n",indexNumber, arr[position[i]]);
-                validationIndex++;
+                printf("\nYou've already assigned this index number -> %d with value -> %d, Please assign a new index number.\n\n",indexNumber, arr[position[i]]);
+                isValidIndex++;
                 break;
             }
         }
 
-        if (validationIndex == 1) continue;
+        if (isValidIndex) continue;
 
-        //value from user
-        printf("enter value : ");
+        //* Value from user
+        printf("Enter a value : ");
         scanf("%d", &value);
 
         arr[indexNumber] = value;
-        position[positionSize] = indexNumber;
-        positionSize++;
+        position[positionIndex] = indexNumber;
+        positionIndex++;
     }
 
-    for (int i = 0; i < positionSize; i++) {
-        printf("index %d <--> value %d\n", position[i], arr[position[i]]);
-    }
+    for (int i = 0; i < positionIndex; i++) printf("index %d <--> value %d\n", position[i], arr[position[i]]);
+
 }
