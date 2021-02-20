d78066b83cfbfbb5018689be97c04dd8760d910c
fahim faisal <fahimfaisal.soikot@gmail.com>
2021-02-20 17:33:52 +0600

    reduce some code

--- Game_of_Throns/Game_of_Thorns.js
+++ Game_of_Throns/Game_of_Thorns.js
@@ -1,16 +1,17 @@
+console.clear()
 const correctTitle = str => 
-    str.split(' ').reduce((correctTitleArr, word) => {
-        let subWord = ''
+    str.split(' ').reduce((correctTitle, word) => {
+        if (/and|the|in|of/i.test(word)) {
+            correctTitle += word.toLowerCase()
+        } else {
+            for (let i = 0; i < word.length; i++)
+                if (!i) correctTitle += word[i].toUpperCase()
+                else correctTitle += word[i].toLowerCase()
+        }
+        correctTitle += ' '
 
-        if (/and|the|in|of/i.test(word)) subWord += word.toLowerCase()
-        else for (let i = 0; i < word.length; i++)
-                if (!i) subWord += word[i].toUpperCase()
-                else subWord += word[i].toLowerCase()
-        
-        correctTitleArr.push(subWord)
-
-        return correctTitleArr
-    }, []).join(' ')
+        return correctTitle
+    }, '')
 
 const str = 'JOn SaNS, tHe kInG oF tHe CounTrY'
 const result = correctTitle(str)
