599d2c96ffc01e7e47e9c5c6dce3d80f3b9c488d
fahimfaisaal <fahimfaisal.soikot@gmail.com>
2021-06-02 04:01:57 +0600

    feat: add deepTrim function

--- RegExp/deepTrim.js
+++ RegExp/deepTrim.js
@@ -1,9 +1,41 @@
-
-function deepTrim(string) {
-    return string.replace(/\s{2,}/g, ' ').trim();
+function cleanExtraSpaces(string) {
+    return string.replace(/\s{2,}/g, ' ');
 }
 
-const multiSpaceStr = `   Hello,        Everyone      i am  fahim
-    faisal    and i am    22        `;
+function fixSymbols(string) {
+    const symbolFixer = [
+        {
+            //* Start symbols
+            regex: /([\(\{\[@#\^])\s(\w+)/g,
+            replace: ' $1$2'
+        },
+        {
+            //* middle symbols
+            regex: /(\w+)\s?([%&\*\+<>\|~\/\-])\s?(\w+)/g,
+            replace: ' $1 $2 $3 '
+        },
+        {
+            //* end symbols
+            regex: /(\w)?\s([,\.\?!;\)\}\]$])/g,
+            replace: '$1$2 '
+        }
+    ]
+ 
+    symbolFixer.forEach(object => {
+        string = string.replace(object.regex, object.replace);
+    });
+  
+    return string;
+}
 
-console.log(deepTrim(multiSpaceStr))
+function deepTrim(string) {
+    const fixSymbolAndSpaces = fixSymbols(cleanExtraSpaces(string));
+
+    return cleanExtraSpaces(fixSymbolAndSpaces).trim();
+}
+
+const crushedStr = `   Hello  ,        Everyone    i am(  fahim
+    faisal        )    and    i'm    22  . who    are     you    ? i have 1000 $  you know   ? 34>10
+   my user name      @    fahimfaisal `;
+
+console.log(deepTrim(crushedStr))
