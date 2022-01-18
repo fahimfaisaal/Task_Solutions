8090643dfbbfb6e055310f5a08fb1f0b8624ae11
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-01-18 18:59:09 +0600

    feat: add set enum

--- TypeScript/sleep.ts
+++ TypeScript/sleep.ts
@@ -1,16 +1,23 @@
-function sleep(time: number, unit?: string): void {
+enum TimeUnit {
+    ms = 'millisecond',
+    sec = 'second',
+    min = 'minute',
+    hr = 'hour'
+}
+
+function sleep(time: number, unit: TimeUnit = TimeUnit.sec): void {
     const units = {
         millisecond: time,
         second: 1e3 * time,
         minute: 1e3 * 60 * time,
         hour: 1e3 * 60 * 60 * time,
     }
-    const selectedTimeLength: number = units[unit] ? units[unit] : units.second;
+    const selectedTimeLength: number = units[unit];
 
     const currentTime: number = new Date().getTime();
     while (currentTime + selectedTimeLength >= new Date().getTime());
 }
 
 console.log('Start the execution');
-sleep(3);
+sleep(10);
 console.log('End the execution');
