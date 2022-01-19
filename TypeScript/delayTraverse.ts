8623a69f9423ef04f35db9afff6210f9c444f5d0
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-01-19 20:17:55 +0600

    feat: add delay funciton in param to delay param

--- TypeScript/delayTraverse.ts
+++ TypeScript/delayTraverse.ts
@@ -1,9 +1,9 @@
-function delayTraverse<T>(array: T[], callback: Function, delay: number = 1) {
+function delayTraverse<T>(array: T[], callback: Function, delay: number | Function = 1) {
     const length: number = array.length;
 
     for (let i = 0; i < length; i++) {
         setTimeout(() => {
             callback(array[i], i, array);
-        }, 1000 * delay * (i + 1))
+        }, 1000 * (typeof delay === 'function' ? delay() : delay) * (i + 1))
     }
 }
