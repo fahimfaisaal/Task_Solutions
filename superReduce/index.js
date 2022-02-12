6589751df8f25f6a8fa27a02d0a822a5e3be3248
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-02-12 18:43:56 +0600

    feat: optimze logic and add benzmark

--- superReduce/index.js
+++ superReduce/index.js
@@ -1,34 +1,13 @@
-
-function isIterableCollection(collection) {
+function isIterableObject(collection) {
   return Symbol.iterator in Object(collection);
 }
 
-function isArguments(collection) {
+function isArgumentsObject(collection) {
   return collection.toString() === '[object Arguments]';
 }
 
 function isExistIndex(collection) {
-  return Array.isArray(collection) || typeof collection === 'string' || isArguments(collection);
-}
-
-/**
- * @param {any} collection iterable or not iterable collection
- * @callback callback
- * @returns 
- */
-function each(collection, callback) {
-  const isIterable = isIterableCollection(collection);
-
-  if (isIterable) {
-    for (const item of collection) {
-      callback(item, isIterable)
-    }
-    return;
-  }
-
-  for (const key in collection) {
-    callback(key, isIterable)
-  }
+  return Array.isArray(collection) || typeof collection === 'string' || isArgumentsObject(collection);
 }
 
 /**
@@ -37,30 +16,44 @@ function each(collection, callback) {
  * @param {*} accumulator 
  * @returns accumulator
  */
-function superReduce(collection, callback, accumulator) {
+function superReduce(collection, callback, accumulator, context = this) {
+  const isIterable = isIterableObject(collection);
   const haveIndex = isExistIndex(collection);
   let indexOrCollection = haveIndex ? 0 : collection;
 
-  each(collection, (keyOrItem, isIterable) => {
-    const callbackArgs = [
-      accumulator,
-      keyOrItem,
-      (isIterable ? indexOrCollection : collection[keyOrItem]),
-      collection
-    ];
+  if (isIterable) {
+    for (const item of collection) {
+      const callbackArgs = [
+        accumulator,
+        item,
+        indexOrCollection,
+        collection
+      ];
 
-    haveIndex ^ isIterable ? callbackArgs.pop() : indexOrCollection++;
-    
-    accumulator = callback(...callbackArgs);
-  })
+      // if collection have doesn't exist index then it pop the last collection
+      haveIndex
+        ? indexOrCollection++
+        : callbackArgs.pop();
+
+      accumulator = callback.apply(context, callbackArgs);
+    }
+
+    return accumulator;
+  }
+
+  // if the collection is not iterable
+  for (const key in collection) {
+    accumulator = callback.call(context, accumulator, key, collection[key], collection);
+  }
 
   return accumulator;
 }
 
-const obj = {
+// * Test cases
+/* const obj = {
   a: 10,
   b: 20,
-  c: 30
+  c: 31
 }
 
 console.log('By Object:', superReduce(obj, (acc, _, value, obj) => {
@@ -72,14 +65,14 @@ const set = new Set([1, 2, 3, 4, 5, 6, 7, 5, 3, 2]);
 
 console.log('By Set:', superReduce(set, (acc, cur, set) => {
   console.log(set)
-  return acc + cur
+  return acc + cur;
 }, 0))
 
 const map = new Map(Object.entries(obj));
 
 console.log('By Map:', superReduce(map, (acc, [_, value], map) => {
   console.log(map)
-  return (acc[value & 1]++, acc)
+  return (acc[value & 1]++, acc);
 }, [0, 0]));
 
 
@@ -89,14 +82,27 @@ console.log('By String:', superReduce(string, (acc, letter, index, string) => {
   if (/\s/.test(letter)) {
     return acc;
   }
-  console.log(string)
+  console.log(index, string)
   return `${acc}${letter}${index === string.length - 1 ? '' : '-'}`;
 }, ''));
 
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-console.log('By Array', superReduce(array, (acc, number, index, array) => {
-  console.log(array, index)
+console.log('By Array:', superReduce(array, (acc, number, index, array) => {
+  console.log(index, array)
   return acc + number ** 2;
-}, 0))
+}, 0)); */
 
+// ! check performance
+/* let stringOfNumber = '';
+for (let i = 0; i < 1e7; i++) {
+  stringOfNumber += ~~(Math.random() * 1000)
+}
+
+console.time("String manipulate with super reduce");
+superReduce(stringOfNumber, (acc, cur, index) => Number(cur ** 2) + (index ? '-' : '') + acc, '');
+console.timeEnd("String manipulate with super reduce");
+
+console.time("String manipulate with builtin reduce");
+stringOfNumber.split('').reduce((acc, cur, index) => Number(cur ** 2) + (index ? '-' : '') + acc, '');
+console.timeEnd("String manipulate with builtin reduce"); */
